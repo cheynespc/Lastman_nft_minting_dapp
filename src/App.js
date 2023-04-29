057a91af0c7863559a8db40a64b9481e8cf2dc86
Cheyne Hayworth <27937312+cheynespc@users.noreply.github.com>
2023-04-30 00:40:09 +1000

    basic page design

--- src/App.js
+++ src/App.js
@@ -215,10 +215,11 @@ function App() {
             }}
           >
             LastManHolding NFT will reward a random existing holder with 10% of the purchase price of last NFT sold.
-            Every NFT will reward randomly until there is one left. Last NFT sold will earn a total of 10% of all the NFTS sold.
-            pricing currently at base cost of 0.21BNB (Beans). this price will increase on every 10 NFTs sold.
-            Get in early to have more chances to be rewarded and remember! You can always have more than one.
-            more chances for rewards. up to 10 can be purchased per transaction.
+            Every NFT purchased will reward a current holder randomly until there is only one left. The last NFT sold will earn a total
+            of 10% of the entire treasury, total value of all the NFTS sold.
+            Pricing is currently at base cost of 0.21BNB (Beans). this price will increase on every 10 NFTs sold.
+            Get in early to have more chances to be rewarded and remember! You can always have more than one, which means
+            more chances for rewards. Up to 10 Lastman NFT's can be purchased per transaction. Enjoy the Ride!!
           </s.TextDescription>
           <s.SpacerSmall />
           <s.TextDescription
